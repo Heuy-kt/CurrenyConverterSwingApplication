57c2522de590262f71660df42e7ab9a327f99769
7devanle <ffamexyz@gmail.com>
2024-08-09 06:34:04 +0100

    completed currency converter swing project

---
+++ src/main/java/com/Heuy/kt/ExchangeMain.java
@@ -0,0 +1,12 @@
+package com.Heuy.kt;
+
+import com.Heuy.kt.swing.CurrencyConverterGUI;
+
+public class ExchangeMain {
+
+	public static void main(String[] args) throws Exception{ 
+		CurrencyConverterGUI currencyConverterGUI = new CurrencyConverterGUI();
+		currencyConverterGUI.convertTheseCurrencies();
+	}
+
+}
---
+++ src/test/java/com/Heuy/kt/CurrencyResponseTest.java
@@ -0,0 +1,17 @@
+package com.Heuy.kt;
+
+import com.Heuy.kt.models.CurrencyResponse;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CurrencyResponseTest {
+
+    @Test
+    void getLocalDateTime() {
+        CurrencyResponse currencyResponse = new CurrencyResponse();
+        assertEquals(currencyResponse.getDateTime(), LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    }
+}
